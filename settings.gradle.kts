45bdc07ba6816d373db72e4a366bc420987eb7fd
MstTanjila <tanjilasweety2020@gmail.com>
2024-06-01 13:46:00 +0600

    Done

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "job_02_MstTanjila_01-06-24"
+include(":app")
+ 
